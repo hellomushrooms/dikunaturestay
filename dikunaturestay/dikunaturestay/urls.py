0b5b7e8b61f23d1ecc21a04cbc604bfff295b152
hellomushrooms <disha.kurkuri@gmail.com>
2025-10-12 13:50:04 +0530

    basic gallery page

--- dikunaturestay/dikunaturestay/urls.py
+++ dikunaturestay/dikunaturestay/urls.py
@@ -20,5 +20,6 @@ from naturestay import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('/gallery', views.gallery, name='gallery'),
 ]
--- dikunaturestay/naturestay/views.py
+++ dikunaturestay/naturestay/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'naturestay/home.html')
+
+def gallery(request):
+    return render(request, 'naturestay/gallery.html')
